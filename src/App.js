0223b6d7b49296adb7e445a6217d862f4990d2cb
Tyler Durham <tdurh13@wgu.edu>
2025-09-15 12:28:01 -0600

    removing unused imports

--- src/App.js
+++ src/App.js
@@ -5,11 +5,7 @@ import { motion } from 'framer-motion';
 import { BrowserRouter as Router, Routes, Route, Link, useLocation } from 'react-router-dom';
 
 // Components
-import Header from './components/Header';
 import About from './components/About';
-import Experience from './components/Experience';
-import Education from './components/Education';
-import Skills from './components/Skills';
 import Projects from './components/Projects';
 import Footer from './components/Footer';
 import Home from './components/Home';
