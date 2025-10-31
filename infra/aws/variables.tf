a9b15b71eb84697d494cd63e6b1e2c056e1e78fa
Tyler Durham <tdurh13@wgu.edu>
2025-10-31 13:28:54 -0600

    updating terraform files

--- infra/aws/variables.tf
+++ infra/aws/variables.tf
@@ -1,9 +1,42 @@
 variable "aws_region" {
+  type    = string
   default = "us-west-2"
 }
-variable "bucket_name" {
-  default = "tyleradurham.com"
+variable "s3_bucket" {
+  type      = string
+  sensitive = true
 }
 variable "domain_name" {
-  default = "tyleradurham.com"
+  type      = string
+  sensitive = true
+}
+variable "route53_zone_id" {
+  description = "Route 53 Zone ID"
+  type        = string
+  sensitive   = true
+}
+variable "kms_key_arn" {
+  description = "Container Registry ARN"
+  type        = string
+  sensitive   = true
+}
+variable "acm_certificate_arn" {
+  description = "CloudFront ARN"
+  type        = string
+  sensitive   = true
+}
+variable "aws_cloudfront_distribution" {
+  description = "CloudFront Distribution"
+  type        = string
+  sensitive   = true
+}
+variable "cloudfront_zone_id" {
+  description = "CloudFront Zone ID"
+  type        = string
+  sensitive   = true
+}
+variable "ecr_repo_name" {
+  description = "ECR Repo Name"
+  type        = string
+  sensitive   = true
 }
